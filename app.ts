bd84357795585703efa6993beea8d9ec2fab9a60
Eric Anderson <eanders@microsoft.com>
2016-10-04 09:00:30 -0700

    Fixed camelCase on textFormat

--- app.ts
+++ app.ts
@@ -17,7 +17,7 @@ var bot = new UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 const sendActivity = (session: Session, activity: any ) => {
-    activity.textformat = session.privateConversationData["textformat"] || "markdown";
+    activity.textFormat = session.privateConversationData["textformat"] || "markdown";
     console.log("sending", activity);
     session.send(new Message(session)
         .text("nominal message")
