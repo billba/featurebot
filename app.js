cb3d634e57eddb235a266762b8ab4eac5333b15a
Bill Barnes <billba@microsoft.com>
2016-10-07 09:25:48 -0700

    support for channelData messages

--- app.js
+++ app.js
@@ -376,4 +376,8 @@ bot.dialog('/', new botbuilder_1.IntentDialog()
     textFormat: "xml",
     text: "Here is some xml text containing <b>bold</b> and <i>italic</i> text."
 }))
-    .onDefault(botbuilder_1.DialogAction.send("valid commands: hero, thumbnail, image, list, carousel, receipt, signin, plain, markdown, xml, typing")));
+    .matches(/^message/i, session => sendActivity(session, {
+    type: "message",
+    text: `You sent me "${session.message.sourceEvent}""`
+}))
+    .onDefault(botbuilder_1.DialogAction.send("valid commands: hero, thumbnail, image, list, carousel, receipt, signin, plain, markdown, xml, typing, message")));
--- app.ts
+++ app.ts
@@ -405,5 +405,11 @@ bot.dialog('/',
             text: "Here is some xml text containing <b>bold</b> and <i>italic</i> text."
         })
     )
-    .onDefault(DialogAction.send("valid commands: hero, thumbnail, image, list, carousel, receipt, signin, plain, markdown, xml, typing"))
+    .matches(/^message/i, session =>
+        sendActivity(session, {
+            type: "message",
+            text: `You sent me "${session.message.sourceEvent}""`
+        })
+    )
+    .onDefault(DialogAction.send("valid commands: hero, thumbnail, image, list, carousel, receipt, signin, plain, markdown, xml, typing, message"))
 );
